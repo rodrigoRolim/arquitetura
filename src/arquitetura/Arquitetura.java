e0aa83c23c1d51620ca2e9a0f1cd593de83a379e
Rodrigo Rolim <rodriggit config --global user.name Rodrigo>
2018-03-14 20:49:32 -0300

    Lucas Santos

--- src/arquitetura/Arquitetura.java
+++ src/arquitetura/Arquitetura.java
@@ -5,6 +5,9 @@
  */
 package arquitetura;
 
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Rodrigo e Lucas Joaquim
@@ -16,6 +19,23 @@ public class Arquitetura {
      */
     public static void main(String[] args) {
         Cliente cliente = new Cliente();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entre com um número 1 para adicionar cliente e 2 para listar todos os clientes");
+        int index = scanner.nextInt();
+        System.out.println(index);
+        switch(index){
+            case 1:
+                System.out.println("Digite o nome do cliente");
+                String name = scanner.nextLine();
+                cliente.addCliente(name);
+                break;
+            case 2:
+                for(String c : cliente.getList()){
+                    System.out.println("Nome " + c);
+                }
+
+        }
+
     }
 
     
