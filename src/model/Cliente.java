a28b4b5ddc887f48d53f5dc69550a0372717028a
Rodrigo Rolim <rodriggit config --global user.name Rodrigo>
2018-03-21 19:59:28 -0300

    Lucas joaquim

---
+++ src/model/Cliente.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+
+/**
+ *
+ * @author Rodrigo e Lucas Joaquim
+ */
+public class Cliente {
+	private String nome;
+	private String telefone;
+	private double limite;
+	private Pais pais;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public double getLimite() {
+		return limite;
+	}
+	public void setLimite(double limite) {
+		this.limite = limite;
+	}
+	public Pais getPais() {
+		return pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	
+}
---
+++ src/model/PaisDAO.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+
+public class PaisDAO {
+	
+	private ArrayList<Pais> paises;
+	
+	public PaisDAO() {
+		this.paises = new ArrayList<Pais>();
+	}
+	
+}
